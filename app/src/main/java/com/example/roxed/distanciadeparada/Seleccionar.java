1e9d5d7c7a6ada0d2e06a4ad5d128f226f7ba4db
DanielJRG <gatonce08@gmail.com>
2015-05-01 23:48:04 -0500

    Pantallas
    Se agregaron las pantallas Ingresar y  Seleccionar

--- app/src/main/java/com/example/roxed/distanciadeparada/Seleccionar.java
+++ app/src/main/java/com/example/roxed/distanciadeparada/Seleccionar.java
@@ -1,44 +1,24 @@
 package com.example.roxed.distanciadeparada;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Seleccionar extends ActionBarActivity {
 
-    private Intent intSesion;
-    private Button seleccionar;
-    private Button ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        seleccionar = (Button) findViewById(R.id.btnseleccionar);
-        ingresar = (Button) findViewById(R.id.btningresar);
-
+        setContentView(R.layout.activity_seleccionar);
     }
 
-    public void onClicSeleccionar(View v) {
-        intSesion = new Intent(this, Seleccionar.class);
-        startActivity(intSesion);
-
-    }
-
-    public void onClicIngresar(View v) {
-        intSesion = new Intent(this, Ingresar.class);
-        startActivity(intSesion);
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_seleccionar, menu);
         return true;
     }
 
